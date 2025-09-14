c790fb6328bdce0e84af874c934dd03ed2601bb9
Mayura21 <mayurasm2000@gmail.com>
2025-09-14 10:23:24 +0530

    updated application file

--- application.py
+++ application.py
@@ -71,4 +71,4 @@ def predict_churn():
 
 if __name__ == '__main__':
     logging.info('Running Flask applicationlication.')
-    application.run(debug=True, host='127.0.0.1', port=5000)
+    application.run(host='0.0.0.0')
